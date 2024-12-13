acbe61c943cf18af772496096f0a472875ab9fab
Vrglab <vrglab1@gmail.com>
2024-12-13 11:07:22 +0100

    Completed my own implementation of a ArrayList using a TDD Format

--- src/main/java/de/szut/mylists/MyArrayList.java
+++ src/main/java/de/szut/mylists/MyArrayList.java
@@ -1,5 +1,72 @@
 package de.szut.mylists;
 
 public class MyArrayList {
+    private int[] array;
 
+    private int active_size;
+
+    public MyArrayList() {
+        array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.MIN_VALUE;
+        }
+        active_size = 0;
+    }
+
+    public int size() {
+        return active_size;
+    }
+
+    public void add(int value) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == Integer.MIN_VALUE) {
+                array[i] = value;
+                active_size++;
+                return;
+            }
+        }
+        extend_array_size();
+        this.add(value);
+    }
+
+    public int get(int index) {
+        return array[index];
+    }
+
+    public void remove(int index) {
+        if(active_size == index + 1) {
+            array[index] = Integer.MIN_VALUE;
+        } else {
+            int to_be_moved = active_size - index;
+
+            for (int i = index; i < to_be_moved; i++) {
+                array[index] = array[i + 1];
+                if(array[i + 2] == Integer.MIN_VALUE) {
+                    array[i + 1] = Integer.MIN_VALUE;
+                    break;
+                }
+            }
+        }
+        active_size--;
+    }
+
+    boolean contains(int value) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void extend_array_size() {
+        int[] newArray = new int[array.length + 1];
+        for (int i = 0; i < newArray.length; i++) {
+            newArray[i] = Integer.MIN_VALUE;
+        }
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+        }
+        array = newArray;
+    }
 }
--- src/test/java/de/szut/mylists/MyArrayListTest.java
+++ src/test/java/de/szut/mylists/MyArrayListTest.java
@@ -1,5 +1,102 @@
 package de.szut.mylists;
 
-public class MyArrayListTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class MyArrayListTest {
+    private MyArrayList myArrayList;
+
+    @BeforeEach
+    public void Init() {
+        myArrayList = new MyArrayList();
+    }
+
+    @Test
+    public void test_Add() {
+        for (int i = 0; i < 3; i++) {
+            myArrayList.add(1);
+        }
+        assertThat(myArrayList.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void test_beyond_ten_Add() {
+        for (int i = 0; i < 12; i++) {
+            myArrayList.add(1);
+        }
+        assertThat(myArrayList.size()).isEqualTo(12);
+    }
+
+    @Test
+    public void test_Size() {
+        myArrayList.add(1);
+        assertThat(myArrayList.size()).isEqualTo(1);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12})
+    public void test_beyond_ten_Contains(int input) {
+        for (int i = 0; i < 12; i++) {
+            myArrayList.add(i + 1);
+        }
+        assertThat(myArrayList.contains(input)).isEqualTo(true);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    public void test_Contains_true(int input) {
+        myArrayList.add(input);
+        assertThat(myArrayList.contains(input)).isEqualTo(true);
+    }
+
+    @Test
+    public void test_Contains_false() {
+        for (int i = 0; i < 12; i++) {
+            myArrayList.add(i + 1);
+        }
+
+        assertThat(myArrayList.contains(15)).isEqualTo(false);
+    }
+
+    @Test
+    public void test_Get() {
+        for (int i = 0; i < 12; i++) {
+            myArrayList.add(i + 1);
+        }
+
+        assertThat(myArrayList.get(4)).isEqualTo(5);
+    }
+
+    @Test
+    public void test_Get_out_of_bounds() throws Exception {
+        for (int i = 0; i < 9; i++) {
+            myArrayList.add(i + 1);
+        }
+
+        assertThatThrownBy(()-> myArrayList.get(15)).isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    @Test
+    public void test_Remove_middle_value() {
+        myArrayList.add(1);
+        myArrayList.add(2);
+        myArrayList.add(3);
+        myArrayList.remove(1);
+        assertThat(myArrayList.get(1)).isEqualTo(3);
+    }
+
+    @Test
+    public void test_Remove_last_value() {
+        myArrayList.add(1);
+        myArrayList.add(2);
+        myArrayList.add(3);
+        myArrayList.remove(2);
+        assertThat(myArrayList.get(2)).isEqualTo(Integer.MIN_VALUE);
+    }
 }
